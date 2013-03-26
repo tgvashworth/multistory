147cd1df928c81d5d9944fa4bc4e4db00e45bd38
Tom Ashworth <tom@phuu.net>
2013-03-26 20:07:18 +0000

    Parse into object.

--- parse.js
+++ parse.js
@@ -1,21 +1,31 @@
-var fs = require('fs');
+var fs = require('fs'),
+    _ = require('underscore');
 
 var file = fs.readFileSync(process.argv[2], {encoding: 'utf8'});
 
-var lines = file.split('\n');
+var lines = file.split('\n'),
+    stories = [];
 
 lines.forEach(function (line) {
 
-  var res = line.match(/([A-Z\s_.,!"'\/$]{2,})[A-Za-z\s_.,!"'\/$]([A-Z\s_.,!"'\/$]{2,})[A-Za-z\s_.,!"'\/$]([A-Z\s_.,!"'\/$]{2,})/g);
+  var res = line.match(/([A-Z\s']+)[A-Za-z\s.,!']([A-Z\s']+)[A-Za-z\s.,!']([A-Z\s']+)/g);
 
   if (!res) return;
 
   var cleaned = res.map(function (segment, index) {
-    var str = (segment || '').trim().replace(',', '');
-    if (index === 0) str = str.replace(' I', '');
+    var str = (segment || '')
+                .replace(',', '')
+                .replace(/as a/i, '')
+                .trim();
+    if (index === 0) str = str.replace(' I', '').trim();
     return str.toLowerCase();
   });
 
-  console.log(cleaned.join(' : '));
+
+  cleaned = cleaned.slice(0,2).concat(cleaned.slice(2).join(' '));
+
+  stories.push(_.object(['who', 'what', 'why'], cleaned));
 
 });
+
+console.log(stories);
